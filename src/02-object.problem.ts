f8ffc0422b191373368f5a6a0a01836918224c6b
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2022-12-08 20:52:36 -0500

    objects

--- src/02-object.problem.ts
+++ src/02-object.problem.ts
@@ -1,14 +1,15 @@
 // CODE
 
-import { expect, it } from "vitest";
-import { z } from "zod";
+import { expect, it } from 'vitest';
+import { z } from 'zod';
 
-const PersonResult = z.unknown();
-//                   ^ 🕵️‍♂️
+const PersonResult = z.object({
+  name: z.string(),
+});
 
 export const fetchStarWarsPersonName = async (id: string) => {
   const data = await fetch(
-    "https://totaltypescript.com/swapi/people/" + id + ".json",
+    'https://totaltypescript.com/swapi/people/' + id + '.json'
   ).then((res) => res.json());
 
   const parsedData = PersonResult.parse(data);
@@ -18,7 +19,7 @@ export const fetchStarWarsPersonName = async (id: string) => {
 
 // TESTS
 
-it("Should return the name", async () => {
-  expect(await fetchStarWarsPersonName("1")).toEqual("Luke Skywalker");
-  expect(await fetchStarWarsPersonName("2")).toEqual("C-3PO");
+it('Should return the name', async () => {
+  expect(await fetchStarWarsPersonName('1')).toEqual('Luke Skywalker');
+  expect(await fetchStarWarsPersonName('2')).toEqual('C-3PO');
 });
